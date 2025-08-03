8b46346b2966dd0a71c3e43889929443ffa70510
Nataliya <mom.natik@gmail.com>
2025-08-03 21:21:12 +0300

    +IsResourcesReadyAsync

--- src/js/resources/controller.mjs
+++ src/js/resources/controller.mjs
@@ -1,10 +1,9 @@
-// import { GetStatusAsync } from "../../../tickettoride-backend-common-js/src/store/game-resources.mjs";
+import { IsResourcesReadyAsync } from "../../../tickettoride-backend-common-js/src/store/provider.mjs";
 
 export async function GetGameStatus(req, res) {
   res.setHeader("Content-Type", "text/plain");
-  const gameId = req.params.gameId;
-
-  res.send(gameId);
+  const isResourcesReady = await IsResourcesReadyAsync(req.params.gameId);
+  res.send(isResourcesReady.toString());
 }
 
 export async function GetMapBackgroundImage(req, res) {
