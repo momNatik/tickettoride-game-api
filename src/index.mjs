61dc70314490ac5cd2fd7dd5ced969ce14f24229
Nataliya <mom.natik@gmail.com>
2025-08-03 19:13:50 +0300

    -cors

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,10 @@
 import express from "express";
-import cors from "cors";
 import { RegisterGameResourcesEndpoints } from "./js/resources/router.mjs";
 
 const port = process.env.PORT;
 
-const corsOptions = { origin: [process.env.TICKETTORIDE_API_URL] };
-
 const app = express();
 
-app.use(cors(corsOptions));
 app.use(express.json());
 
 RegisterGameResourcesEndpoints(app);
