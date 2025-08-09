e72753f3617f5bcad9735998851f84ad61660175
Nataliya <mom.natik@gmail.com>
2025-08-09 20:37:08 +0300

    workable GetMapBackgroundEndpoint

--- src/js/resources/controller.mjs
+++ src/js/resources/controller.mjs
@@ -1,4 +1,8 @@
-import { IsResourcesReadyAsync } from "../../../tickettoride-backend-common-js/src/store/provider.mjs";
+import { IsResourcesReadyAsync } from "../../../tickettoride-backend-common/src/db/provider.mjs";
+import {
+  GetFileAsync,
+  GetFilesByPrefixAsync,
+} from "../../../tickettoride-backend-common/src/file-store/game-files.mjs";
 
 export async function GetGameStatus(req, res) {
   res.setHeader("Content-Type", "text/plain");
@@ -8,8 +12,63 @@ export async function GetGameStatus(req, res) {
 
 export async function GetMapBackgroundImage(req, res) {
   res.setHeader("Content-Type", "image/png");
-
   const gameId = req.params.gameId;
+  const key = GetFileStoreKey("background", gameId);
+  const fileResponse = await GetFileAsync(key);
 
-  res.send(gameId);
+  res.writeHead(200, {
+    "Content-Type": "image/jpeg",
+    "Content-Length": fileResponse.ContentLength,
+  });
+
+  const buffer = await fileResponse.Body.transformToByteArray();
+  res.end(buffer);
+}
+
+export async function GetMapTopology(req, res) {
+  res.setHeader("Content-Type", "image/png");
+  const gameId = req.params.gameId;
+  const key = GetFileStoreKey("topology", gameId);
+  const file = await GetFileAsync(key);
+
+  res.send(file);
+}
+
+export async function GetAvatar(req, res) {
+  res.setHeader("Content-Type", "image/png");
+  const gameId = req.params.gameId;
+  const userId = req.params.userId;
+  const key = GetFileStoreKey("avatar", gameId, userId);
+  const file = await GetFileAsync(key);
+
+  res.send(file);
+}
+
+export async function GetAvatars(req, res) {
+  res.setHeader("Content-Type", "image/png");
+  const gameId = req.params.gameId;
+  const key = GetFileStoreKey("avatar", gameId);
+  const files = await GetFilesByPrefixAsync(key);
+  console.log(`Users list: ${files}`);
+  res.send(files);
+}
+
+function GetFileStoreKey(resourseType, ...ids) {
+  const prefix = GetFileStoreKeyPrefix(ids);
+  const resourceName = GetResourceName(resourseType);
+  return `${prefix}${resourceName}`;
+}
+
+function GetFileStoreKeyPrefix(...ids) {
+  const notEmptyIds = ids.filter((id) => id).map((id) => `${id}_`);
+  return notEmptyIds.join("");
+}
+
+function GetResourceName(type) {
+  const resourceNames = {
+    background: "map_background.png",
+    topology: "map_topology.svg",
+    avatar: "user_avatar.jpg",
+  };
+  return resourceNames[type];
 }
--- src/js/resources/router.mjs
+++ src/js/resources/router.mjs
@@ -1,6 +1,15 @@
-import { GetGameStatus, GetMapBackgroundImage } from "./controller.mjs";
+import {
+  GetGameStatus,
+  GetMapBackgroundImage,
+  GetMapTopology,
+  GetAvatars,
+  GetAvatar,
+} from "./controller.mjs";
 
 export function RegisterGameResourcesEndpoints(app) {
-    app.get("/:gameId/resources/status", GetGameStatus);
-    app.get("/:gameId/resources/mapbackgroundimage", GetMapBackgroundImage);
+  app.get("/:gameId/status", GetGameStatus);
+  app.get("/:gameId/map/background", GetMapBackgroundImage);
+  app.get("/:gameId/map/topology", GetMapTopology);
+  app.get("/:gameId/users/avatar", GetAvatars);
+  app.get("/:gameId/users/:userId/avatar", GetAvatar);
 }
