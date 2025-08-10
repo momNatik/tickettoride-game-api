a281c9b326404094878a7f67a018f0c8a5e305c4
Nataliya <mom.natik@gmail.com>
2025-08-10 13:59:48 +0300

    import provider > db

--- src/js/game/controller.mjs
+++ src/js/game/controller.mjs
@@ -1,13 +1,12 @@
-import { IsResourcesReadyAsync } from "../../../tickettoride-backend-common/src/db/provider.mjs";
-import { CreateGameAsync as abc } from "../../../tickettoride-backend-common/src/db/provider.mjs";
+import DB from "../../../tickettoride-backend-common/src/db/provider.mjs";
 
 export async function CreateGameAsync(req, res) {
- await abc(req.body);
+ await DB.CreateGameAsync(req.body);
  res.send(200);
 }
 
 export async function GetGameStatusAsync(req, res) {
   res.setHeader("Content-Type", "text/plain");
-  const isResourcesReady = await IsResourcesReadyAsync(req.params.gameId);
+  const isResourcesReady = await DB.IsResourcesReadyAsync(req.params.gameId);
   res.send(isResourcesReady.toString());
 }
